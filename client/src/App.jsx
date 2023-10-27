7a639fe6027bc1c787d6f8d509f555bf4578f26a
Pavan <kolipaka.pavan.kp@gmail.com>
2023-10-27 16:10:14 +0530

    modified:   client/src/App.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,71 @@
-import React from "react";
+// import React from "react";
+// import { DigiVC, Form, PrintDigiVC } from "./components";
+// import "./App.css";
+// import { StateContextProvider } from "./context/StateContext";
+
+// const App = () => {
+//   return (
+//     <StateContextProvider>
+//       {" "}
+//       <div
+//         style={{
+//           display: "flex",
+//           flexDirection: "row",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <input
+//           type="color"
+//           name="color-picker"
+//           id="color-picker"
+//           onChange={(e) => console.log(e.target.value)}
+//         />
+//         <Form />
+//         {/* <DigiVC /> */}
+//         <PrintDigiVC />
+//       </div>
+//     </StateContextProvider>
+//   );
+// };
+
+// export default App;
+import React, { useState, useEffect } from "react";
 import { DigiVC, Form, PrintDigiVC } from "./components";
 import "./App.css";
 import { StateContextProvider } from "./context/StateContext";
 
 const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState("#f0f0f0"); // Initial background color
+
+  const handleColorChange = (e) => {
+    setBackgroundColor(e.target.value);
+  };
+
+  // Use useEffect to update the body background color when the state changes
+  useEffect(() => {
+    document.body.style.backgroundColor = backgroundColor;
+  }, [backgroundColor]);
+
   return (
     <StateContextProvider>
-      {" "}
       <div
         style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
           alignItems: "center",
+          width: "100%",
+          minHeight: "100vh", // Ensure the background covers the entire viewport
         }}
       >
+        <input
+          type="color"
+          name="color-picker"
+          id="color-picker"
+          value={backgroundColor}
+          onChange={handleColorChange}
+        />
         <Form />
         {/* <DigiVC /> */}
         <PrintDigiVC />
--- client/src/components/PrintDigiVC.jsx
+++ client/src/components/PrintDigiVC.jsx
@@ -1,4 +1,32 @@
-import React, { useRef } from "react";
+// import React, { useRef } from "react";
+// import { useReactToPrint } from "react-to-print";
+// import DigiVC from "./DigiVC"; // Import your DigiVC component
+
+// const PrintDigiVC = () => {
+//   const componentRef = useRef();
+//   const handlePrint = useReactToPrint({
+//     content: () => componentRef.current,
+//     onBeforeGetContent: () => {
+//       // Set the zoom level to 65%
+//       document.documentElement.style.zoom = "65%";
+//     },
+//     onAfterPrint: () => {
+//       // Reset the zoom level to 100% after printing
+//       document.documentElement.style.zoom = "100%";
+//     },
+//   });
+
+//   return (
+//     <div>
+//       <DigiVC ref={componentRef} />
+//       <button onClick={handlePrint}>Print DigiVC</button>
+//     </div>
+//   );
+// };
+
+// export default PrintDigiVC;
+
+import React, { useRef, useEffect } from "react";
 import { useReactToPrint } from "react-to-print";
 import DigiVC from "./DigiVC"; // Import your DigiVC component
 
@@ -15,6 +43,18 @@ const PrintDigiVC = () => {
       document.documentElement.style.zoom = "100%";
     },
   });
+
+  useEffect(() => {
+    // Get the background color of the body element
+    const bodyBackgroundColor = window.getComputedStyle(
+      document.body
+    ).backgroundColor;
+
+    // Apply the body background color to the component's style
+    if (componentRef.current) {
+      componentRef.current.style.backgroundColor = bodyBackgroundColor;
+    }
+  }, []);
 
   return (
     <div>
